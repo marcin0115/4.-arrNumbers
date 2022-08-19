55bdaadfcd02c54f3944f5ea91a6f149f98220c6
marcin0115 <marcin0115@op.pl>
2022-08-19 08:12:28 +0200

    first commit

---
+++ arrayNumbers.js
@@ -0,0 +1,42 @@
+const randomNumber = (length, min, max) => {
+  const newArray = [...Array(length)].map(() =>
+    Math.floor(Math.random() * (max - min + 1) + min)
+  );
+  return newArray;
+};
+
+//a) Napisz funkcję, która zwróci tablicę o długości podanej jako „howManyNumbers”. Ta tablica musi zawierać w sobie losowe liczby z zakresu min i max:
+const genArrayNumbers = (howManyNumbers = 10, min = 1, max = 10) => {
+  randomNumber(howManyNumbers, min, max);
+};
+genArrayNumbers();
+
+//b) Napisz funkcje, która wygeneruje array z 10 arrayami
+const genNewArray = (howManyArrays = 10, length = 10, min = 1, max = 10) => {
+  const allArrays = [...Array(howManyArrays)].map(() =>
+    randomNumber(length, min, max)
+  );
+  return allArrays;
+};
+genNewArray();
+
+// STARA WERSJA:
+// const genArrayNumbers = (howManyNumbers = 10, min = 1, max = 10) => {
+//   randomArray = (length, min, max) =>
+//     [...Array(length)].map(() =>
+//       Math.floor(Math.random() * (max - min + 1) + min)
+//     );
+//   // return randomArray(howManyNumbers, min, max);
+// };
+// genArrayNumbers();
+
+// const genNewArray = (howManyArrays = 10, length = 10, min = 1, max = 10) => {
+//   arrayWithArrays = [...Array(howManyArrays)].map(() =>
+//     [...Array(length)].map(() =>
+//       //dubluje sie, przeniesc na gore i uzyc w obydwu funkcjach:
+//       Math.floor(Math.random() * (max - min + 1) + min)
+//     )
+//   );
+//   // return arrayWithArrays;
+// };
+// genNewArray();
